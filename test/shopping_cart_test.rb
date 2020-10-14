a8f15fcb80ebdf806fd913d3cbccd846257b2f4d
Grayson Cunningham <gecun289@gmail.com>
2020-10-14 13:47:03 -0600

    Add additional tests for shopping_cart class

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,6 +6,10 @@ require './lib/product'
 class ShoppingCartTest < Minitest::Test
   def setup
     @cart = ShoppingCart.new("King Soopers", "30items")
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    @product4 = Product.new(:produce, 'apples', 0.99, '20')
   end
 
   def test_it_exists
@@ -19,16 +23,47 @@ class ShoppingCartTest < Minitest::Test
   end
 
   def test_add_products
-    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-    product2 = Product.new(:meat, 'chicken', 4.50, '2')
-    @cart.add_product(product1)
-    @cart.add_product(product2)
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
 
-    assert_equal [product1, product2], @cart.products
+    assert_equal [@product1, @product2], @cart.products
   end
 
   def test_cart_details
     expected = {name: "King Soopers", capacity: 30}
     assert_equal expected, @cart.details
+  end
+
+  def test_total_number_of_products
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal 13, @cart.total_number_of_products
+  end
+
+  def test_is_full?
+    skip
+    assert_equal false, @cart.is_full?
+
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal false, @cart.is_full?
+
+    @cart.add_product(@product4)
+
+    assert_equal true, @cart.is_full?
+  end
+
+  def test_products_by_category
+    skip
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+    @cart.add_product(@product4)
+
+    assert_equal [@product1, @product3], @cart.products_by_category(:paper)
   end
 end

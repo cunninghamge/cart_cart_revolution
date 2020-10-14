0596b66e943166cfb4319585654e9e95fd894643
Grayson Cunningham <gecun289@gmail.com>
2020-10-14 13:23:58 -0600

    Create ShoppingCart class and test files

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,22 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/shopping_cart'
+require './lib/product'
+
+class ShoppingCartTest < Minitest::Test
+  def setup
+    @cart = ShoppingCart.new("King Soopers", "30items")
+  end
+
+  # assert_instance_of ShoppingCart, @cart
+  # assert_equal "King Soopers", @cart.name
+  # assert_equal 30, @cart.capacity
+  # assert_equal [], @cart.products
+
+  # product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+  # product2 = Product.new(:meat, 'chicken', 4.50, '2')
+  # @cart.add_product(product1)
+  # @cart.add_product(product2)
+  # assert_equal [product1, product2], @cart.products
+  # assert_equal {name: "King Soopers", capacity: 30}, @cart.details
+end
